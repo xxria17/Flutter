77ec7742cc877d85b34d4b32e53175295d3944a0
leedh <leedh@leedh6520.local>
2022-01-13 10:36:59 +0900

    Practice Network

---
+++ http_example/lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HttpApp(),
+    );
+  }
+}
+
+class HttpApp extends StatefulWidget {
+  const HttpApp({Key? key}) : super(key: key);
+
+  @override
+  _HttpApp createState() => _HttpApp();
+}
+
+class _HttpApp extends State<HttpApp> {
+  String result = '';
+  late List data;
+  late TextEditingController _editingController;
+  late ScrollController _scrollController;
+  int page = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    data = List.empty(growable: true);
+    _editingController = TextEditingController();
+    _scrollController = new ScrollController();
+
+    _scrollController.addListener(() {
+      if (_scrollController.offset >=
+      _scrollController.position.maxScrollExtent &&
+      !_scrollController.position.outOfRange) {
+        print('bottom');
+        page++;
+        getJSONData();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _editingController,
+          style: TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(hintText: '검색어를 입력하세요.'),
+        )
+      ),
+      body: Container(
+        child: Center(
+          child: data.length == 0
+          ? Text(
+              '데이터가 없습니다.',
+              style: TextStyle(fontSize: 20),
+          textAlign: TextAlign.center,
+          ) : ListView.builder(itemBuilder: (context, index) {
+            return Card(
+              child: Container(
+                child: Row(
+                  children: <Widget>[
+                    Image.network(
+                      data[index]['thumbnail'],
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.contain,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width - 150,
+                          child: Text(
+                            data[index]['title'].toString(),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Text('저자 : ${data[index]['authors'].toString()}'),
+                        Text('가격 : ${data[index]['sale_price'].toString()}'),
+                        Text('판매중 : ${data[index]['status'].toString()}'),
+                      ],
+                    )
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.start,
+                )
+              ),
+            );
+          }, itemCount: data.length,
+          controller: _scrollController,)
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          page = 1;
+          data.clear();
+          getJSONData();
+        },
+        child: Icon(Icons.file_download),
+      ),
+    );
+  }
+
+  Future<String> getJSONData() async {
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&page=$page&query=${_editingController.value.text}';
+    var response = await http.get(Uri.parse(url),
+    headers: {"Authorization": "KakaoAK b304ca92815ace3a98d4a2ea46cf16da"});
+    setState(() {
+      var dataConvertedToJSON = json.decode(response.body);
+      List result = dataConvertedToJSON['documents'];
+      data.addAll(result);
+    });
+
+    return response.body;
+  }
+}
+
+
+
